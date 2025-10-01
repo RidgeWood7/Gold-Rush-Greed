da80e45ee355132d9ff9f73d372ded948ef00c9d
MUKILTEO\1602247 <abel4prezz@gmail.com>
2025-10-01 11:08:44 -0700

    <forgot what I did here>

---
+++ Assets/Kreston/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed = 2f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 movementDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = movementDirection * movementSpeed;
+    }
+}
