23db5ea03a4e000652ef26a1ff07183efbc8e285
MUKILTEO\1602247 <abel4prezz@gmail.com>
2025-10-02 13:12:49 -0700

    <started woking on the GameManager and edited the map for future testing>

---
+++ Assets/Kreston/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Inventory:")]
+    //Inventory Bools For if the player has an item or not
+    //public bool has
+    [SerializeField] private bool _hasPan;
+    [SerializeField] private bool _hasSluiceBox;
+    [SerializeField] private bool _hasPick;
+    [SerializeField] private bool _hasHorseFeed;
+    [Header("Transport:")]
+
+    //bools for transport unlocked
+    [SerializeField] private bool _hasHorse;
+    [SerializeField] private bool _hasStagecoach;
+    [SerializeField] private bool _hasTrain;
+    [Header("Regions:")]
+    //bools for unlocked regions
+    [Header("River")]
+    [SerializeField] private bool _unlockedRiver;
+    [SerializeField] private BoxCollider2D _boxColliderRiver;
+    [Header("Mines")]
+    [SerializeField] private bool _unlockedMines;
+    [SerializeField] private BoxCollider2D _boxColliderMines;
+    [Header("Fields")]
+    [SerializeField] private bool _unlockedFields;
+    [SerializeField] private BoxCollider2D _boxColliderFields;
+    [Header("Ints:")]
+
+    //Ints of the amount of things the player has
+    [SerializeField] private int _wheelsCollected;
+    [SerializeField] private int _wheelsInInv;
+    [SerializeField] private int _coal;
+    [SerializeField] private int _oil;
+    [SerializeField] private int _gold;
+    [Header("Multipliers:")]
+    [SerializeField] private int _multDust = 1;
+    [SerializeField] private int _multIngot = 8;
+    [SerializeField] private int _multDrilled = 22;
+    public int weight;
+
+    //Region Checkers
+    private void Update()
+    {
+        //Mines
+        if (_unlockedMines){
+
+        }
+    }
+
+    public void Collecting(int goldAmount, int weightAdding, string type)
+    {
+        weight += weightAdding;
+
+        if (type == "Dust")
+        {_gold += goldAmount * _multDust;}
+
+        else if (type == "Ingot")
+        {_gold += goldAmount * _multIngot;}
+
+        else if (type == "Drilled")
+        {_gold += goldAmount * _multDrilled;}
+
+        else
+        {Debug.Log("There was not a correct string name, please enter either: \"Dust\" \"Ingot\" \"Drilled\" ");}
+    }
+}
