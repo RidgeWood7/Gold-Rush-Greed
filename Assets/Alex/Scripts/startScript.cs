04a0e19c4a93b83547e1fa488ab89fd55420ab19
MUKILTEO\1725205 <AlexanderSmith2026@outlook.com>
2025-10-08 12:12:40 -0700

    Started work on start scene and created final scene

---
+++ Assets/Alex/Scripts/startScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startScript : MonoBehaviour
+{
+   
+
+    private void changeToMainScript()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+}
