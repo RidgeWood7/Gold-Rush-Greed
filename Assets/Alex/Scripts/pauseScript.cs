54cf570a026779c242ca5cdc09e645d3f4dc1874
BIRKYY\Alexs <bubblybirky@gmail.com>
2025-10-08 18:54:50 -0700

    Worked on pause screen, still not working but general design done

--- Assets/Alex/Scripts/pauseScript.cs
+++ Assets/Alex/Scripts/pauseScript.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class pauseScript : MonoBehaviour
 {
+
+    public TMPro.TMP_Text tipsText;
+
+    public List<string> toolTips;
+
+    private bool pauseOpened = true;
+
+    public Canvas pauseCanvas; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tipsText.text = toolTips[Random.Range(0, toolTips.Count)];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("1");
+            if (!pauseOpened)
+            {
+                Debug.Log("2");
+                pauseOpened = true;
+                tipsText.text = toolTips[Random.Range(0, toolTips.Count)];
+                pauseCanvas.gameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("3");
+                pauseOpened = false;
+                pauseCanvas.gameObject.SetActive(false);
+            }
+
+        }
     }
+
+
+
+
+
+
 }
