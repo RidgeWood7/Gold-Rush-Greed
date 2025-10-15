be0224874ad98b54a30d819c235dc589e0e69704
MUKILTEO\1725205 <AlexanderSmith2026@outlook.com>
2025-10-15 13:13:48 -0700

    HORSE? (added functionaliity to the horse button)

--- Assets/Alex/Scripts/startScript.cs
+++ Assets/Alex/Scripts/startScript.cs
@@ -4,7 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class startScript : MonoBehaviour
 {
-   
+
+    [SerializeField] private AudioSource neighAudio;
+    [SerializeField] private AudioClip loud;
+    [SerializeField] private AudioClip notLoud;
+
 
     public void changeToMainScript()
     {
@@ -16,6 +20,22 @@ public class startScript : MonoBehaviour
     {
         Application.Quit();
     }
+
+
+    public void neigh()
+    {
+        if (Random.Range(0, 4) == 0)
+        {
+            neighAudio.clip = loud;
+        }
+        else
+        {
+            neighAudio.clip = notLoud;
+        }
+        neighAudio.Play();
+    }
+
+
 
 
 
